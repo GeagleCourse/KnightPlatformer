210ffaae8cff0541e2b09b3ed7283d6371fffe62
Haetham <halhaddad@geagle.tech>
2021-08-26 12:22:37 +0300

    Created a bunch of directories, imported assets and added a background to Level 1 scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D myrigidBody;
+
+    float horizontalDirection;
+    public float playerSpeed = 5f, jumpForce = 10;
+
+    Vector2 playerMovement; 
+    Animator myAnimator;
+    SpriteRenderer myRenderer;
+    bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        MovePlayer();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isJumping == false)
+            {
+                StartCoroutine(Jump());
+            }
+        }
+
+    }
+
+    void MovePlayer()
+    {
+
+        horizontalDirection = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            playerSpeed = 15f;
+        else
+            playerSpeed = 5f;
+
+        playerMovement = new Vector2(horizontalDirection * playerSpeed, 0f);
+        myrigidBody.velocity = playerMovement;
+
+        if (horizontalDirection > 0)
+        {
+            myAnimator.SetBool("isWalking", true);
+            myRenderer.flipX = false;
+        }
+        else if (horizontalDirection < 0)
+        {
+            myAnimator.SetBool("isWalking", true);
+            myRenderer.flipX = true;
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
+    }
+
+    IEnumerator Jump()
+    {
+        isJumping = true;
+        myrigidBody.velocity = new Vector2(0f, jumpForce);
+        yield return new WaitForSeconds(1f);
+        isJumping = false; 
+    }
+
+}
